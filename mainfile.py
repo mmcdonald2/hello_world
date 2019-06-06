b169a9c939d67a0df8fef9b844e124c1a054b8ed
mmcdonald2 <47544580+mmcdonald2@users.noreply.github.com>
2019-06-06 14:20:29 -0400

    more stuff

--- mainfile.py
+++ mainfile.py
@@ -1,7 +1,13 @@
 #matthew mcdonald
+#hello world test program
 #6/6/19
 
 print("Hello world")
 
-for i in range(10):
+for i in range(20):
   print(i)
+  
+for i in range(100):
+  for a in range(100):
+    for b in range(100):
+      print(i*a*b)
